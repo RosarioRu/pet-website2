12ce9958d5a06b7773916f0c48cca2c089f24115
RosarioRu <mariarruval@gmail.com>
2022-03-31 10:09:26 -0700

    Add code to hide light-mode button when clicked and switch to ligth mode again.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("button#dark").click(function() {
     $("body").addClass("dark-mode");
+    $("button#dark").hide();
+    $("button#light").show();
   });
 
   $("button#light").click(function() {
+    $("button#light").hide();
+    $("button#dark").show();
     $("body").removeClass();
     $("body").adddClass("light-mode");
+    // $("button#light").hide();
+    // $("button#dark").show();
+    
   });
 
 });
