7e590a21ce3bc7786548789d11dc868ae902c357
RosarioRu <mariarruval@gmail.com>
2022-03-31 09:37:57 -0700

    Add to function to remove dark mode class and add light-mode class when button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("button#dark").click(function() {
     $("body").addClass("dark-mode");
   });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").adddClass("light-mode");
+  });
+
 });
